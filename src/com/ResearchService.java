7a6a014d31c7c45b40566f93f57ab309f9a9647d
SasankaDidula <61312369+SasankaDidula@users.noreply.github.com>
2021-04-20 17:00:47 +0530

    insert, update, delete functions in research service implemented

--- src/com/ResearchService.java
+++ src/com/ResearchService.java
@@ -16,7 +16,9 @@ import org.jsoup.nodes.Document;
 
 @Path("/Researchs")
 public class ResearchService {
+
 	Research researchObj = new Research();
+
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
@@ -24,4 +26,54 @@ public class ResearchService {
 	{
 		return researchObj.readResearchs();
 	}
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertResearch(@FormParam("researchCode") String researchCode,
+			@FormParam("researchName") String researchName,
+			@FormParam("researchPrice") String researchPrice,
+			@FormParam("researchDesc") String researchDesc)
+	{
+		String output = researchObj.insertResearch(researchCode, researchName, researchPrice, researchDesc);
+		return output;
+	}
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateResearch(String researchData)
+	{
+		//Convert the input string to a JSON object
+		JsonObject researchObject = new JsonParser().parse(researchData).getAsJsonObject();
+		//Read the values from the JSON object
+		String researchID = researchObject.get("researchID").getAsString();
+		String researchCode = researchObject.get("researchCode").getAsString();
+		String researchName = researchObject.get("researchName").getAsString();
+		String researchPrice = researchObject.get("researchPrice").getAsString();
+		String researchDesc = researchObject.get("researchDesc").getAsString();
+		String output = researchObj.updateResearch(researchID, researchCode, researchName, researchPrice, researchDesc);
+		return output;
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteResearch(String researchData)
+	{
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(researchData, "", Parser.xmlParser());
+
+		//Read the value from the element <researchID>
+		String researchID = doc.select("researchID").text();
+		String output = researchObj.deleteResearch(researchID);
+		return output;
+	}
+
+
+
+
 }
